7e7cb8603372bd7da1b7d5665ad932c42f1bc147
DungNTHE163521/SEP490-G71-FA24 <dungnthe163521@fpt.edu.vn>
2024-10-23 22:04:38 +0700

    update

---
+++ RMS-Client/Service/BuildingService.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using RMS_API.DTOs;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using RMS_API.Models;
+
+namespace RMS_Client.Services
+{
+    public class BuildingService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BuildingService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<BuildingDTO>> GetAllBuildingsAsync()
+        {
+            var response = await _httpClient.GetAsync("https://localhost:7056/api/Building/GetAllBuildings");
+            response.EnsureSuccessStatusCode();
+
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<BuildingDTO>>(jsonResponse);
+        }
+    }
+}
