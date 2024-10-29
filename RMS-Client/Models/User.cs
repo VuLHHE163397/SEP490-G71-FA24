59d09c68ff13802114f0b78459b970793ca0b082
tuyennqhe161645 <tuyennqhe161645@fpt.edu.vn>
2024-10-29 09:35:30 +0700

    Update BE build

---
+++ RMS-Client/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RMS_API.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Buildings = new HashSet<Building>();
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string MidName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public int Phone { get; set; }
+        public int UserStatusId { get; set; }
+        public int RoleId { get; set; }
+
+        public virtual Role Role { get; set; } = null!;
+        public virtual UserStatus UserStatus { get; set; } = null!;
+        public virtual ICollection<Building> Buildings { get; set; }
+    }
+}
--- RMS-Client/obj/Debug/net6.0/RMS_Client.AssemblyInfo.cs
+++ RMS-Client/obj/Debug/net6.0/RMS_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RMS_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f81305ccf65d8c9ce10687a489207828ad21dc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71fbb0878d58e368c3fcd620d05595340954d4e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RMS_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RMS_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
