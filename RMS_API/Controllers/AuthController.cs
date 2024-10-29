3548906846b45e40e7216fb833737b6218cb374d
tuyennqhe161645 <tuyennqhe161645@fpt.edu.vn>
2024-10-29 09:33:33 +0700

    Add API/AuthController.cs
    add action login, register. add function GennerateJwtToken. Add model register and login

--- RMS_API/Controllers/AuthController.cs
+++ RMS_API/Controllers/AuthController.cs
@@ -1,60 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using RMS_API.Models;
-using RMS_Client.Models;
 using System.ComponentModel.DataAnnotations;
-using System.Diagnostics;
-using System.Security.Claims;
-using System.Text;
-using System.Security.Claims;
 
-namespace RMS_Client.Controllers
+namespace RMS_API.Controllers
 {
-    
-    public class HomeController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
     {
-       
-        private readonly ILogger<HomeController> _logger;
+
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
         private readonly IConfiguration _configuration;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Home()
-        {
-            return View();
-        }
-        public IActionResult ListRoom()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        public IActionResult Login()
-        {
-            return View("Views/Login/Login.cshtml");
-        }
-
-        public IActionResult Register()
-        {
-            return View("Views/Login/Register.cshtml");
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public HomeController(UserManager<User> userManager, SignInManager<User> signInManager, IConfiguration configuration)
+        public AuthController(UserManager<User> userManager, SignInManager<User> signInManager, IConfiguration configuration)
         {
             _userManager = userManager;
             _signInManager = signInManager;
