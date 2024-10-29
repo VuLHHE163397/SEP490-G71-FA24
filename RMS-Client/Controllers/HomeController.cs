1ca3d7813b5adc0c64163f34f7c0abcab5120d79
tuyennqhe161645 <tuyennqhe161645@fpt.edu.vn>
2024-10-29 13:32:34 +0700

    Update

--- RMS-Client/Controllers/HomeController.cs
+++ RMS-Client/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using RMS_API.Models;
 using RMS_Client.Models;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
@@ -14,10 +13,7 @@ namespace RMS_Client.Controllers
     public class HomeController : Controller
     {
        
-        private readonly ILogger<HomeController> _logger;
-        private readonly UserManager<User> _userManager;
-        private readonly SignInManager<User> _signInManager;
-        private readonly IConfiguration _configuration;
+        private readonly ILogger<HomeController> _logger;      
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -53,82 +49,6 @@ namespace RMS_Client.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        public HomeController(UserManager<User> userManager, SignInManager<User> signInManager, IConfiguration configuration)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _configuration = configuration;
-        }
-
-        [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] RegisterModel model)
-        {
-            var user = new User { FirstName = model.Username, LastName = model.Fullname };
-            var result = await _userManager.CreateAsync(user, model.Password);
-
-            if (result.Succeeded)
-            {
-                return Ok(new { result = "User registered successfully" });
-            }
-
-            return BadRequest(result.Errors);
-        }
-
-        [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] LoginModel model)
-        {
-            var user = await _userManager.FindByNameAsync(model.Username);
-            if (user == null || !await _userManager.CheckPasswordAsync(user, model.Password)) return Unauthorized();
-            //var token = GenerateJwtToken(user);
-            var token = "test";
-            return Ok(new { token });
-
-        }
-
-        //private string GenerateJwtToken(User user)
-        //{
-        //    var claims = new[]
-        //    {
-        //    new Claim(JwtRegisteredClaimNames.Sub, Guid.NewGuid().ToString()),
-        //    new Claim(JwtRegisteredClaimNames.Name, user.firstName),
-        //    new Claim("firstName", user.firstName ?? String.Empty),
-        //};
-
-        //    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
-        //    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-        //    var token = new JwtSecurityToken(
-        //        issuer: _configuration["Jwt:Issuer"],
-        //        audience: _configuration["Jwt:Audience"],
-        //        claims: claims,
-        //        expires: DateTime.Now.AddHours(24),
-        //        signingCredentials: creds);
-
-        //    return new JwtSecurityTokenHandler().WriteToken(token);
-        //}
-        public class RegisterModel
-        {
-            [Required]
-            [DataType(DataType.Text)]
-            public string Username { get; set; }
-            [Required]
-            [DataType(DataType.Text)]
-            public string Fullname { get; set; }
-            [Required]
-            [DataType(DataType.Password)]
-            public string Password { get; set; }
-        }
-
-        public class LoginModel
-        {
-            [Required]
-            [DataType(DataType.Text)]
-            public string Username { get; set; }
-
-            [Required]
-            [DataType(DataType.Password)]
-            public string Password { get; set; }
-        }
+        
     }
 }
