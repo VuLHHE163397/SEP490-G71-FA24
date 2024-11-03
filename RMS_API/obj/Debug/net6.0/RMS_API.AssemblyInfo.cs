3dd3975dbd9c25d6037c23e293e91fa9b70c4844
Le Hoang Vu <vulhhe163397@fpt.edu.vn>
2024-11-03 18:37:16 +0700

    Update new model

--- RMS_API/obj/Debug/net6.0/RMS_API.AssemblyInfo.cs
+++ RMS_API/obj/Debug/net6.0/RMS_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RMS_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4154f7d30fea026b5bf95a74f951c9807104237b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f730753cfbf3bfe8dd183218bff12f3daee8ec44")]
 [assembly: System.Reflection.AssemblyProductAttribute("RMS_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RMS_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
