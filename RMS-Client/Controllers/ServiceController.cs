8c4636146ed8c3c993323fd021c0a1cee2e4ba55
DungNTHE163521/SEP490-G71-FA24 <dungnthe163521@fpt.edu.vn>
2024-11-05 12:28:11 +0700

    update

--- RMS-Client/Controllers/ServiceController.cs
+++ RMS-Client/Controllers/ServiceController.cs
@@ -1,15 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RMS_API.DTOs;
+using System.Net.Http.Headers;
 
 namespace RMS_Client.Controllers
 {
     public class ServiceController : Controller
     {
+        private readonly HttpClient client = null;
+        private string ServiceApiUri = "https://localhost:7056/api/Service";
+        
+
+        public ServiceController()
+        {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+
+        }
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult ListService()
         {
+            string apiUrlService = ServiceApiUri + "/GetAllServie";
+            var buildings = new List<ServiceDTO>();
             return View();
         }
         public IActionResult AddService()
