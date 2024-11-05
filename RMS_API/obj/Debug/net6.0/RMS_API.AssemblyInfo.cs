87e3f76b229f074f7555ec39552c871d91fe702c
Le Hoang Vu <vulhhe163397@fpt.edu.vn>
2024-11-05 10:37:21 +0700

    new update js in listfavouriteroom

--- RMS_API/obj/Debug/net6.0/RMS_API.AssemblyInfo.cs
+++ RMS_API/obj/Debug/net6.0/RMS_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RMS_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f730753cfbf3bfe8dd183218bff12f3daee8ec44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd3975dbd9c25d6037c23e293e91fa9b70c4844")]
 [assembly: System.Reflection.AssemblyProductAttribute("RMS_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RMS_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
