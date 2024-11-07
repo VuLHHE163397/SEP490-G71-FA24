0ee86659467f182d83ebe8b34ccb7e4c00d7fff5
tuyennqhe161645 <tuyennqhe161645@fpt.edu.vn>
2024-11-07 10:37:48 +0700

    FIx login check for hash password

---
+++ RMS-Client/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static RMS_API.Controllers.AuthController;
+
+namespace RMS_Client.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View("~/Views/Login/Register.cshtml");
+        }
+        public IActionResult Login()
+        {
+            return View("~/Views/Login/Login.cshtml");
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel userLogin)
+        {            
+            return RedirectToAction("ListBuilding", "Building");
+        }
+    }
+
+   
+}
