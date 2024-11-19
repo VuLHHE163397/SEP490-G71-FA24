444acc7808c0acc66a3e3400d963db263ab8368a
tuyennqhe161645 <tuyennqhe161645@fpt.edu.vn>
2024-11-19 23:30:54 +0700

    Fix db, dbcontext, BE

---
+++ RMS_API/DTOs/BillDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace RMS_API.DTOs
+{
+    public class BillDTO
+    {
+
+        public int BillId { get; set; }
+        public string BuildingName { get; set; } = null!;
+        public string RoomNumber { get; set; } = null!;
+        public string TenantName { get; set; } = null!;
+        public decimal TotalAmount { get; set; }
+        public decimal PaidAmount { get; set; }
+        public decimal RemainingAmount { get; set; }
+    }
+}
