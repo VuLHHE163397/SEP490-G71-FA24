64358b081309575a319f5ff348c86171ae847dc4
Le Hoang Vu <vulhhe163397@fpt.edu.vn>
2024-11-20 19:10:03 +0700

    Update AddBuildingDTO

--- RMS_API/DTOs/AddBuildingDTO.cs
+++ RMS_API/DTOs/AddBuildingDTO.cs
@@ -10,11 +10,12 @@
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
         public int ProvinceId { get; set; }
-        public string? ProvinceName { get; set; } 
+        public string? ProvinceName { get; set; }
         public string DistrictName { get; set; } = null!;
         public string WardName { get; set; } = null!;
         public string AddressDetails { get; set; } = null!;
         public string BuildingStatus { get; set; } = null!;
-        
+        public int UserId { get; set; }
+
     }
 }
