c72e603abbe3580ccc2c788decf2b269205c665e
Le Hoang Vu <vulhhe163397@fpt.edu.vn>
2024-11-20 19:18:52 +0700

    Big Update

---
+++ RMS_API/Models/ServicesOfRoom.cs
@@ -0,0 +1,11 @@
+﻿namespace RMS_API.Models
+{
+    public class ServicesOfRoom
+    {
+        public int RoomId { get; set; }
+        public int ServiceId { get; set; }
+
+        public virtual Room Room { get; set; } = null!;
+        public virtual Service Service { get; set; } = null!;
+    }
+}
