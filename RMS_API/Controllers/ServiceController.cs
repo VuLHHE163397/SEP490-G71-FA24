f55970d91a6ce1000d0b6647677f0e9381c20745
DungNTHE163521/SEP490-G71-FA24 <dungnthe163521@fpt.edu.vn>
2024-11-22 14:24:08 +0700

    commit

--- RMS_API/Controllers/ServiceController.cs
+++ RMS_API/Controllers/ServiceController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RMS_API.DTOs;
@@ -105,6 +106,7 @@ namespace RMS_API.Controllers
         }
         //Xóa dịch vụ
         [HttpDelete]
+        [Authorize(Roles = "Landlord")]
         public async Task<IActionResult> DeleteAsync(int id)
         {
             try
