cb3055a9750ce62f719561370db544cc42a6e37f
tuyennqhe161645 <tuyennqhe161645@fpt.edu.vn>
2024-11-25 12:22:21 +0700

    Update viewProfile and editProfile
    Add 2 fileds: FacebookUrl and ZaloUrl

--- RMS_API/DTOs/ProfileDTO.cs
+++ RMS_API/DTOs/ProfileDTO.cs
@@ -8,5 +8,7 @@
         public string MidName { get; set; } = null!;
         public string Email { get; set; } = null!;       
         public string Phone { get; set; } = null!;
+        public string? FacebookUrl { get; set; }
+        public string? ZaloUrl { get; set; }
     }
 }
