b2869b507b71021974df21d5ae6b6a81e7e32c25
Le Hoang Vu <vulhhe163397@fpt.edu.vn>
2024-12-03 14:44:57 +0700

    Update Login + Service + Facility

--- RMS_API/Controllers/MaintainanceRequestController.cs
+++ RMS_API/Controllers/MaintainanceRequestController.cs
@@ -12,6 +12,7 @@ namespace RMS_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = "Landlord")]
     public class MaintainanceRequestController : ControllerBase
     {
         private readonly RMS_SEP490Context _context;
